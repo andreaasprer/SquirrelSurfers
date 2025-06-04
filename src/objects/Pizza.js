34b9d2b68eb3b358590f3ec6fa1881d30004e9ad
andreaasprer <140675071+andreaasprer@users.noreply.github.com>
2025-06-03 21:41:31 -0700

    better pizza model

--- src/objects/Pizza.js
+++ src/objects/Pizza.js
@@ -2,12 +2,11 @@ import Snack from './Snack';
 
 export default class Pizza extends Snack {
     constructor(scene) {
-        super(scene, '../../models/pizza.glb', 1.3, -.9);
+        super(scene, '../../models/pizza.glb', .4, -.3);
     }
 
     // override the parent class's model loaded callback
     onModelLoaded(model) {
-        model.rotation.z = -Math.PI / 8; // tilt a bit
-        model.rotation.y = Math.PI / 2;
+        model.rotation.z = -Math.PI / 4; // tilt a bit
     }
 }
